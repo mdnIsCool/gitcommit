21e6e306a2692c5736e2f7bd807b550f19f41bcb
mdnIsCool <93033350+mdnIsCool@users.noreply.github.com>
2021-11-11 19:10:51 +0100

    added an array that gets logged to console with a loop (#2)
    Co-authored-by: Fredi Fox <[igorstudy56@gmail.com]>

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 const button = document.querySelector(".button");
 const div = document.querySelector(".div");
+const array1 = [1, 2, 3, 4];
+
+array1.forEach((number) => {
+  console.log(number);
+});
